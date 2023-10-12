6cfde6eafcc2cd8997b4de96c72bf69008498a20
Emily Whetter <emlynfelix@gmail.com>
2023-10-12 22:02:50 +0100

    Stopped tests clearing DB

--- tests/Feature/APITest.php
+++ tests/Feature/APITest.php
@@ -2,11 +2,13 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class APITest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * @dataProvider providerAPIData
      */ 
